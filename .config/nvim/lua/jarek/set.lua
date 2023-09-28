928ddd6ad470f20c0a13baada2eb50d7c1aedb1e
jaroslavszkandera <jaroslav.szk@gmail.com>
2023-09-28 23:15:34 +0200

    Comment delete

--- .config/nvim/lua/jarek/set.lua
+++ .config/nvim/lua/jarek/set.lua
@@ -1,16 +1,14 @@
--- fat cursor
--- vim.opt.guicursor = ""
-
 -- line numbers and relative line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
 -- indenting
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+-- automatically wrap text to another line
 vim.opt.wrap = true
 
 -- smart wrap
@@ -19,14 +17,16 @@ vim.opt.wrap = false
 -- forbid backups
 vim.opt.swapfile = false
 vim.opt.backup = false
+
 -- undotree for days
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
--- non-highlighting incremental search
+-- highlighting incremental search
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
+-- good colors
 vim.opt.termguicolors = true
 
 -- never have less than 8 line from bottom/top
@@ -37,6 +37,5 @@ vim.opt.isfname:append("@-@")
 -- fast update time
 vim.opt.updatetime = 50
 
--- vim.opt.colorcolumn = "80"
-
+-- enable system clipboard
 vim.o.clipboard = "unnamedplus"
