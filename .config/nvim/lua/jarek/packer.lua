4b2c2245a893b93b31775e734e8916a9dcc58d67
jaroslavszkandera <jaroslav.szk@gmail.com>
2024-07-12 16:46:23 +0200

    remove inlay hints, disable copilot by default

--- .config/nvim/lua/jarek/packer.lua
+++ .config/nvim/lua/jarek/packer.lua
@@ -1,15 +1,10 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
-    -- or                            , branch = '0.1.x',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
@@ -21,11 +16,23 @@ return require('packer').startup(function(use)
     end
   })
 
+  -- use ({'catppuccin/nvim',
+  --       as = 'catppucin',
+  --     config = function()
+  --     vim.cmd('colorscheme catppuccin-latte')
+  --   end
+  -- })
+
   use{'neoclide/coc.nvim', branch = 'release'}
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use('christoomey/vim-tmux-navigator')
   use('tpope/vim-commentary')
-  use('github/copilot.vim')
+  use {
+    'github/copilot.vim',
+    config = function()
+      vim.g.copilot_enabled = false
+    end
+  }
 end)
--- .config/nvim/lua/jarek/remap.lua
+++ .config/nvim/lua/jarek/remap.lua
@@ -20,11 +20,12 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 -- format the code
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- TODO
 -- navigate quickfix or location list
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- vim.keymap.set("n", "<C-k>", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>cprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
