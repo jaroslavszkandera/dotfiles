8a35120d6795f5aed6708a0c23b9c1084c98da1a
Jaroslav Szkandera <jaroslav.szk@gmail.com>
2025-08-28 16:27:56 -0500

    Fix ruff formatter not working

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -8,7 +8,7 @@ return {
 				formatters_by_ft = {
 					lua = { "stylua" },
 					cpp = { "clang-format" },
-					python = { "ruff" },
+					python = { "isort", "ruff_format" },
 				},
 				format_on_save = {
 					lsp_fallback = true,
